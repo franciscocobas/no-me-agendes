dc7241eea25a28392f6f43c55330fc4180b09693
Francisco Cobas <fcarocena@gmail.com>
2023-11-07 17:15:18 -0300

    Improve app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,15 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <input className='border border-sky-500 text-gray-900 px-2 py-1' type="tel" name="tel" id="" value={tel} onChange={(e) => {
-        console.log(e.target.value)
+      <h1 className=' text-xl font-bold '>No me agendes</h1>
+      <p className=' w-96 text-center '>Aplicación para mandar mensajes de WhatsApp sin agendar contactos.</p>
+      <p className=' w-96 text-center '>Escribí un número uruguayo y apretá Enviar, se abrirá whatsapp para escribirle a ese número.</p>
+      <form onSubmit={openWhatsapp} className='flex flex-col mt-4'>
+        <input className='border border-sky-500 text-gray-900 px-2 py-1' type="tel" name="tel" id="" value={tel} onChange={(e) => {
         setTel(e.target.value)
-        }} />
-        <button className='bg-slate-300 p-2 rounded-sm mt-4' onClick={openWhatsapp}>Enviar</button>
+        }} placeholder='099 123 123' />
+        <button type='submit' className='bg-slate-300 p-2 rounded-sm mt-4'>Enviar</button>
+      </form>
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
