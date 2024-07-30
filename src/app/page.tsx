b66236da788736d57b606d64045e5761143873fc
Francisco Cobas <fcarocena@gmail.com>
2024-07-30 18:01:54 -0300

    Add select text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 "use client"
-import {  useState } from "react"
+import {  FormEvent, useRef, useState } from "react"
 
 
 export default function Home() {
   const [tel, setTel] = useState<string>("");
   const [isInternationalTel, setInternationalTel] = useState<boolean>(false);
+  const inputEle = useRef<any>(null);
 
-  function openWhatsapp() {
+  function openWhatsapp(e: any) {
+    e.preventDefault();
     if (isInternationalTel) {
       window.open(`https://wa.me/${tel.replaceAll(" ", "").replaceAll("-", "")}`, "_blank");
     } else {
       window.open(`https://wa.me/+598${tel.replaceAll(" ", "").slice(1)}`, "_blank");
     }
-    setTel("");
+    inputEle.current?.select();
   }
 
   return (
@@ -28,9 +30,10 @@ export default function Home() {
       }
       <p className='mt-3 text-'>Hacer click <button className="underline" onClick={() => setInternationalTel(!isInternationalTel)}>acá</button> si tengo un número {isInternationalTel ? "de Uruguay" : "internacional"}.</p>
       <form onSubmit={openWhatsapp} className="flex flex-col mt-4 md:max-w-72 md:mx-auto">
-        <input className="border border-sky-500 text-gray-900 px-2 py-1 rounded-sm" type="tel" name="tel" id="" value={tel} onChange={(e) => {
-        setTel(e.target.value)
-        }} placeholder={ isInternationalTel ? "+54 9 3887 40-0837" : "099 123 123" } />
+        <input className="border border-sky-500 text-gray-900 px-2 py-1 rounded-sm" type="tel" name="tel" id="" value={tel} 
+          onChange={(e) => setTel(e.target.value)} placeholder={ isInternationalTel ? "+54 9 3887 40-0837" : "099 123 123" }
+          ref={inputEle}
+        />
         <button type="submit" className="bg-slate-300 p-2 rounded-sm mt-4">Enviar</button>
       </form>
     
